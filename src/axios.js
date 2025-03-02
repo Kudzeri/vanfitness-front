2d32aefbca2ce1f033082b7eb2e6ba5a4f72971b
Kudzeri <kudzeri@gmail.com>
2025-03-02 22:52:48 +0500

    ...

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://vanfitness-api-production.up.railway.app/",
+  baseURL: "https://vanfitness-api-production.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
